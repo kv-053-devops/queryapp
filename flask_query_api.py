a5b40350c13a64c6d7a1dd35bc7ca45dec47e2c5
ansh-shmyg <52212182+ansh-shmyg@users.noreply.github.com>
2019-08-28 22:49:25 +0300

    Update flask_query_api.py
    change lines order for class obeject

--- flask_query_api.py
+++ flask_query_api.py
@@ -3,28 +3,6 @@ import yaml
 import requests
 #from .query_class import Query
 
-
-app = Flask(__name__)
-
-
-@app.route('/query/yml_data', methods=['POST'])
-def receive_yml_data():
-    if request.method == 'POST':
-
-        yml_req_src = request.get_data(cache=True, as_text=True)
-        yml_data = yaml.load(yml_req_src,Loader=yaml.FullLoader)
-        query_obj = Query(yml_data['symbols'],yml_data['query_type'])
-        if query_obj.query_type == 'intraday':
-            query_obj.day_range = yml_data['day_range']
-            query_obj.time_interval = yml_data['time_interval']
-
-        yml_response = query_obj.make_query()
-
-        return yml_response
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5003')
-
 class Query(object):
     def __init__(self,symbols,query_type,day_range = 0,time_interval = 0):
         self.config_api_url = "http://cfgmanapp.dev.svc:5004/conf/query"
@@ -58,3 +36,23 @@ class Query(object):
         # print(res.json())
         return res.json()
 
+app = Flask(__name__)
+
+
+@app.route('/query/yml_data', methods=['POST'])
+def receive_yml_data():
+    if request.method == 'POST':
+
+        yml_req_src = request.get_data(cache=True, as_text=True)
+        yml_data = yaml.load(yml_req_src,Loader=yaml.FullLoader)
+        query_obj = Query(yml_data['symbols'],yml_data['query_type'])
+        if query_obj.query_type == 'intraday':
+            query_obj.day_range = yml_data['day_range']
+            query_obj.time_interval = yml_data['time_interval']
+
+        yml_response = query_obj.make_query()
+
+        return yml_response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5003')
